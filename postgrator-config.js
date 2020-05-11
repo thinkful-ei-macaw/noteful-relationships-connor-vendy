c07debbebd782c04c006ceb37f7da39848fa458b
Connor McCormack <mccormack.connor.m@gmail.com>
2020-05-10 21:26:29 -0500

    deployed

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 
 module.exports = {
-  migrationsDirectory: 'migrations',
-  driver: 'pg',
-  connectionString:
+  "migrationsDirectory": 'migrations',
+  "driver": 'pg',
+  "connectionString":
     process.env.NODE_ENV === 'test'
       ? process.env.TEST_DATABASE_URL
       : process.env.DATABASE_URL,
+  "ssl": !!process.env.SSL,
 };
 
 //  ssl: !!process.env.SSL
